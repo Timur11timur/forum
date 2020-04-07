65141ff2449c746b25073382ce924f62db3ca38f
Timur <timur@fralik.ru>
2020-04-07 23:54:14 +0300

    After 1 video

---
+++ app/Http/Controllers/ThreadController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Thread;
+use Illuminate\Http\Request;
+
+class ThreadController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Thread  $tread
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Thread $tread)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Thread  $tread
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Thread $tread)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Thread  $tread
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Thread $tread)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Thread  $tread
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Thread $tread)
+    {
+        //
+    }
+}
