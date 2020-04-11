69bbc94b4a00cb446cdef8b4c62f2b885287e402
Timur <timur@fralik.ru>
2020-04-11 18:30:13 +0300

    After 3 video

---
+++ tests/Feature/ReadThreadsTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Reply;
+use App\Thread;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ReadThreadsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->thread = factory(Thread::class)->create();
+    }
+
+    /** @test */
+    public function a_user_can_view_all_threads()
+    {
+        $response = $this->get('/threads');
+
+        $response->assertSee($this->thread->title);
+    }
+
+    /** @test */
+    public function a_user_can_browse_a_single_thread()
+    {
+        $response = $this->get('/threads/' . $this->thread->id);
+
+        $response->assertSee($this->thread->title);
+    }
+
+    /** @test */
+    public function a_user_can_read_replies_that_are_associated_with_a_thread()
+    {
+        $reply = factory(Reply::class)->create(['thread_id' => $this->thread->id]);
+
+        $response = $this->get('/threads/' . $this->thread->id);
+
+        $response->assertSee($reply->body);
+    }
+}
