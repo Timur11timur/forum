c10f8fcd2232c999a1984609ed0ad004db3cf94d
Timur <timur@fralik.ru>
2020-05-06 00:09:12 +0300

    After 27 video

--- tests/Feature/ProfilesTest.php
+++ tests/Feature/ProfilesTest.php
@@ -23,11 +23,11 @@ class ProfilesTest extends TestCase
     /** @test */
     public function profiles_display_all_threads_created_by_the_associated_user()
     {
-        $user = factory(User::class)->create();
+       $this->signIn();
 
-        $thread = factory(Thread::class)->create(['user_id' => $user->id]);
+        $thread = factory(Thread::class)->create(['user_id' => auth()->user()->id]);
 
-        $this->get("/profiles/" . $user->name)
+        $this->get("/profiles/" . auth()->user()->name)
             ->assertSee($thread->title)
             ->assertSee($thread->body);
     }
